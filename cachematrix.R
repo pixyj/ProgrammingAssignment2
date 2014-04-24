0ae601f55f2267d16a196e71c2235c0298421275
pramodliv1 <pramodliv1@gmail.com>
2014-04-25 00:04:10 +0530

    Assignment: Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,107 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Functions:
 
-## Write a short comment describing this function
+#makeCacheMatrix: Creates a cachable matrix
+
+#cacheSolve: Returns inverse of matrix
+
+#closeEnough:  Checks if two matrices are "identical" enough. Used for testing
+                #as product of matrix and its inverse doesn't always yield integer 1s and 0s
+
+#testCacheSolve:  Runs test case for a random rnorm matrix for square n*n matrix
+
+
+
+#Creates a special matrix that can cache its inverse
+#Similar to the one in the assignment example
 
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
 
+    get <- function() {
+        x
+    }
+
+    setInverse <- function(i) {
+        inverse <<- i
+    }
+
+    getInverse <- function() {
+        inverse
+    }
+    list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+#Accepts a cached matrix created using makeCacheMatrix as input and returns inverse. 
+#If value is cached, uses cached value instead of recomputing the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(!is.null(inverse)) {
+        message("Getting cached Data")
+        return(inverse)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setInverse(i)
+    i
 }
+
+#Checks if two matrices are good enough to be considered equal
+#Used for testing 
+
+closeEnough <- function(a, b, maxDiff) {
+
+    diff <- a - b
+    vectorDiff <- as.vector(diff)
+
+    for(value in vectorDiff) {
+        if(abs(value) > maxDiff) {
+            return(FALSE)
+        }
+    }
+
+    TRUE
+
+}
+
+#Check if inverse works correctly for a random n * n square matrix
+#And also tests if inverse is cached
+
+testCacheSolve <- function(n) {
+    
+    #Create a random n * n square matrix and initialize the cache
+    x <- matrix(rnorm(n * n), nrow=n, ncol=n)
+    print(x)
+    
+    cachedMatrix <- makeCacheMatrix(x)
+
+    inverse <- cacheSolve(cachedMatrix)
+    product <- x %*% inverse
+    
+    testResult <- TRUE
+
+    #Test if inverse works correctly
+    print(product)
+    if(!closeEnough(product, diag(n), 0.001)) {
+        message("Error in inverse calculation")
+        testResult <- FALSE
+    }
+
+    #Test if value is cached
+    cachedInverse <- cachedMatrix$getInverse()
+
+    if(!identical(inverse, cachedInverse)) {
+        message("Error: Value not cached")
+        testResult <- FALSE
+    }
+
+    testResult
+}
+
